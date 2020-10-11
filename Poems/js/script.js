d7ba647715890d9d08f7056b33a7734ae57f8a99
daniilkolasnikov1 <72712055+daniilkolasnikov1@users.noreply.github.com>
2020-10-12 00:46:08 +0300

    Add files via upload

---
+++ Poems/js/script.js
@@ -0,0 +1,71 @@
+$(document).ready(function() { 
+$('.Authors').hide();
+  $('#butnAuthor').click(function() {/* Делаем отрытие вкладки с авторами на нажатие кнопки автор */
+  $(".Authors").slideToggle(200);
+  });
+});
+
+$(document).ready(function() {
+$('#Bunin').hide();
+  $('#butnBunin').click(function() { /* Делаем открытие стихов бунина  */ 
+  $("#Bunin").slideToggle(500);
+  });
+});
+
+$(document).ready(function() {
+$('#Tutchev').hide();
+  $('#butnTutchev').click(function() { /* Делаем открытие стихов тютчева */ 
+  $("#Tutchev").slideToggle(500);
+  });
+});
+
+$(document).ready(function() {
+$('#Vanshenkin').hide();
+  $('#butnVanshenkin').click(function() { /* Делаем открытие стихов ваншенкина */ 
+  $("#Vanshenkin").slideToggle(500);
+  });
+});
+
+$(document).ready(function() {
+$('#Nekrasov').hide();
+  $('#butnNekrasov').click(function() { /* Делаем открытие стихов  некрасова */ 
+  $("#Nekrasov").slideToggle(500);
+  });
+});
+
+$(document).ready(function() {
+$('#Pasternak').hide();
+  $('#butnPasternak').click(function() { /* Делаем открытие стихов пастернака */ 
+  $("#Pasternak").slideToggle(500);
+  });
+});
+$(document).ready(function() {
+$('#BuninPoem').hide();
+  $('#BuninPoem-name').click(function() { /* Делаем открытие стихов пастернака */ 
+  $("#BuninPoem").slideToggle(500);
+  });
+});
+$(document).ready(function() {
+$('#TutchevPoem').hide();
+  $('#TutchevPoem-name').click(function() { /* Делаем открытие стихов пастернака */ 
+  $("#TutchevPoem").slideToggle(500);
+  });
+});
+$(document).ready(function() {
+$('#VanshenkinPoem').hide();
+  $('#VanshenkinPoem-name').click(function() { /* Делаем открытие стихов пастернака */ 
+  $("#VanshenkinPoem").slideToggle(500);
+  });
+});
+$(document).ready(function() {
+$('#NekrasovPoem').hide();
+  $('#NekrasovPoem-name').click(function() { /* Делаем открытие стихов пастернака */ 
+  $("#NekrasovPoem").slideToggle(500);
+  });
+});
+$(document).ready(function() {
+$('#PasternakPoem').hide();
+  $('#PasternakPoem-name').click(function() { /* Делаем открытие стихов пастернака */ 
+  $("#PasternakPoem").slideToggle(500);
+  });
+});
